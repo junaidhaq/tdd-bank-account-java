f113253358bdcaec3ad768814b6274850575ccf6
junaidhaq <74965465+junaidhaq@users.noreply.github.com>
2021-01-28 15:32:42 +0000

    adding withrawal tests

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -8,6 +8,21 @@ public class Account {
     int balance = 0;
 
     public void deposit(int amount) {
+
+        if (amount < 0) {
+            throw new UnsupportedOperationException("Deposit of negative amount not permitted.");
+        }
+
         balance += amount;
+    }
+
+    public void withdrawal(int amount) {
+
+        if (balance >= amount) {
+            balance -= amount;
+            return;
+        }
+
+        throw new UnsupportedOperationException("Account does not contain enough funds to complete withdrawal.");
     }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -3,6 +3,7 @@ package org.xpdojo.bank;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AccountTest {
 
@@ -28,12 +29,52 @@ public class AccountTest {
     }
 
     @Test
+    public void depositNegativeAmount() {
+        Account account = new Account();
+
+        Exception exception = assertThrows(UnsupportedOperationException.class, () -> {
+            account.deposit(-1);
+        });
+
+        String expectedMessage = "Deposit of negative amount not permitted.";
+        String actualMessage = exception.getMessage();
+
+        assertThat(actualMessage).isEqualTo(expectedMessage);
+    }
+
+    @Test
     public void withdrawAnAmountToDecreaseTheBalance() {
 
-//        Account account = new Account();
-//        account.deposit(100);
-//        account.with(100);
-//
-//        assertThat(account.getBalance()).isEqualTo(amountOf(4.0d));
+        Account account = new Account();
+        account.deposit(100);
+        account.withdrawal(60);
+
+        assertThat(account.getBalance()).isEqualTo(40);
+    }
+
+    @Test
+    public void withdrawFullAmountFromAccount() {
+
+        Account account = new Account();
+        account.deposit(100);
+        account.withdrawal(100);
+
+        assertThat(account.getBalance()).isEqualTo(0);
+    }
+
+    @Test
+    public void withdrawAmountGreaterThanAvailableInAccount() {
+
+        Account account = new Account();
+        account.deposit(100);
+
+        Exception exception = assertThrows(UnsupportedOperationException.class, () -> {
+            account.withdrawal(101);
+        });
+
+        String expectedMessage = "Account does not contain enough funds to complete withdrawal.";
+        String actualMessage = exception.getMessage();
+
+        assertThat(actualMessage).isEqualTo(expectedMessage);
     }
 }
