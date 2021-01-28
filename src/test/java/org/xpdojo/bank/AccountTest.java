be3fb5e52307201be359aeb4b6a2d9c69c2e71ba
junaidhaq <74965465+junaidhaq@users.noreply.github.com>
2021-01-28 15:16:05 +0000

    Update AccountTest.java

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -18,4 +18,22 @@ public class AccountTest {
         account.deposit(100);
         assertThat(account.getBalance()).isEqualTo(100);
     }
+
+    @Test
+    public void depositMultipleAmounts() {
+        Account account = new Account();
+        account.deposit(100);
+        account.deposit(200);
+        assertThat(account.getBalance()).isEqualTo(300);
+    }
+
+    @Test
+    public void withdrawAnAmountToDecreaseTheBalance() {
+
+//        Account account = new Account();
+//        account.deposit(100);
+//        account.with(100);
+//
+//        assertThat(account.getBalance()).isEqualTo(amountOf(4.0d));
+    }
 }
